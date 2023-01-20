4baa78ebb570ac2c649ce69a587efad90ec5429b
Louis_Grant <ljsgrant@gmail.com>
2023-01-20 10:17:58 +0000

    add auth helper functions

---
+++ src/lib/auth.js
@@ -0,0 +1,27 @@
+const setToken = (token) => localStorage.setItem('token', token);
+
+const getToken = () => {
+  return localStorage.getItem('token');
+};
+
+const getPayload = () => {
+  const token = getToken();
+
+  if (!token) {
+    return false;
+  }
+
+  const tokenParts = getToken().split('.');
+
+  if (tokenParts.length < 3) {
+    return false;
+  }
+
+  return JSON.parse(Buffer.from(tokenParts[1], 'base64'));
+};
+
+const deleteToken = () => {
+  localStorage.removeItem('token');
+};
+
+export const AUTH = { setToken, getToken, getPayload, deleteToken };
