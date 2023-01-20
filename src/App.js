b08e408e3442b9447ded616aa7813976bd6b15b3
Louis_Grant <ljsgrant@gmail.com>
2023-01-20 00:05:24 +0000

    working on BirdList: get all birds & map over results

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import BirdList from './components/BirdList';
 
 function App() {
-  axios
-    .get('/api/birds/')
-    .then(({ data }) => {
-      console.log(data);
-    })
-    .catch((err) => console.error(err));
-
   return (
     <div id='app'>
       <Router>
         <Navbar />
         <Routes>
           <Route path='/' element={<div>Home</div>} />
-          <Route path='/birds' element={<BirdList/>} />
+          <Route path='/birds' element={<BirdList />} />
         </Routes>
       </Router>
     </div>
--- src/components/BirdList.js
+++ src/components/BirdList.js
@@ -1,9 +1,22 @@
-export default function BirdList(){
+import { API } from '../lib/api';
+import { useState, useEffect } from 'react';
 
+export default function BirdList() {
+  const [birdData, setBirdData] = useState(null);
 
-  return(
-    <div className="BirdList">
-      Bird List
+  useEffect(() => {
+    API.GET(API.ENDPOINTS.allBirds)
+      .then(({ data }) => {
+        setBirdData(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className='BirdList'>
+      {birdData?.map((bird) => (
+        <p key={bird.id}>{bird.name}</p>
+      ))}
     </div>
-  )
+  );
 }
